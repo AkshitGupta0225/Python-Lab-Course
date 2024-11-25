67e65783c7c012ef00c555b9b03293b112eca56e
AkshitGupta0225 <akshitgupta20005@gmail.com>
2024-11-25 16:34:53 +0530

    Add files via upload

---
+++ Lab 8/6_ Binary_Search.py
@@ -0,0 +1,16 @@
+def binary_search(arr, target):
+    low, high = 0, len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+# Example Usage
+array = [10, 20, 30, 40, 50]
+target = 30
+print("Element found at index:", binary_search(array, target))
